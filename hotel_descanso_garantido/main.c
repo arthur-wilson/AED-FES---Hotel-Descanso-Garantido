495501b13c614bb096a918cfa024f600ce2e2793
arthurwilson707 <85751689+arthurwilson707@users.noreply.github.com>
2021-06-30 11:13:09 -0300

    created basic structure

--- hotel_descanso_garantido/main.c
+++ hotel_descanso_garantido/main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
-    printf("Hotel Descanso Garantido\n");
+    setlocale(LC_ALL, "portuguese");
+
+    int user_menuOption;
+
+    do
+    {
+        printf("-------| Hotel Descanso Garantido |-------\n\n");
+
+        printf("\t1 - Cadastrar Cliente\n");
+        printf("\t2 - Cadastrar Funcionário\n");
+        printf("\t3 - Cadastrar Estadia\n");
+        printf("\t4 - Cadastrar Quarto\n");
+
+        printf("\nDigite o número da opção desejada: ");
+        scanf("%i", &user_menuOption);
+        fflush(stdin);
+
+        switch(user_menuOption)
+        {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default:
+                printf("\n**Opção Inválida! Tente novamente**");
+                getchar();
+                fflush(stdin);
+        }
+        system("cls");
+
+    } while(user_menuOption != 0);
+
+
     return 0;
 }
